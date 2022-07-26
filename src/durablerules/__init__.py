9f5e0b1d36f6f2cef5ad6e68e3d32e3966ccc389
Simon Suthers <simonsuthers@yahoo.co.uk>
2022-07-26 14:56:01 +0100

    Added assert rule

--- src/durablerules/__init__.py
+++ src/durablerules/__init__.py
@@ -1,10 +1,54 @@
-#from durable.lang import ruleset, post, when_all, m, c, assert_fact, post_batch, count
-from durable.lang import *
+from durable.lang import ruleset, post, when_all, m, assert_fact, post_batch, count
+#from durable.lang import *
 import pandas as pd
 import json
 
 # see https://pypi.org/project/durable-rules/
 # https://github.com/jruizgit/rules/blob/master/docs/py/reference.md
+# https://www.npmjs.com/package/durable/v/0.36.95
+
+#############################################################################################################
+# basic test using hard coded values
+
+with ruleset("test"):
+    # antecedent
+    @when_all(m.ISIN == "World")
+    def say_hello(c):
+        print(f"Basic rule: ISIN is {c.m.ISIN}")
+
+test_data = [{'ISIN': 'World'}, {'ISIN': 'World1'}]
+
+for data in test_data:
+    try:
+        post('test', data)
+    except:
+        print(f"Basic rule: ISIN is not {data['ISIN']}")
+
+#############################################################################################################
+# basic test using hard coded values
+
+with ruleset('animal'):
+    # will be triggered by 'Kermit eats flies'
+    @when_all((m.country == 'ca'))
+    def frog(c):
+        dict1 = {'ISIN': c.m.ISIN, 'exchange': 'toronto' }
+        c.assert_fact(dict1)
+
+    @when_all((m.country != 'ca'))
+    def frog(c):
+        c.assert_fact({'ISIN': c.m.ISIN, 'exchange': 'not known' })
+
+    @when_all(+m.exchange)
+    def output(c):
+        print('Fact: {0} {1} {2}'.format(c.m.ISIN, c.m.country, c.m.exchange))
+
+test_data = [{'ISIN': 'CA001', 'country': 'ca'}, {'ISIN': 'UK002', 'country': 'uk'}]
+
+for data in test_data:
+    try:
+        assert_fact('animal', data)
+    except:
+        print(f"{data['ISIN']} errored")
 
 #############################################################################################################
 
@@ -21,66 +65,66 @@ post('risk', {'ID': 9, 'ISIN': 'IS0003', 'Date': '2022-07-07T00:00:00.000Z', 'Pr
 '''
 
 #############################################################################################################
-# import testing workbook
-df = pd.read_excel("durablerules\\durable_rules_test.xlsx")
-dict = json.loads(df.to_json(orient='records', date_format="iso"))
-print(dict)
+# # import testing workbook
+# df = pd.read_excel("durablerules\\durable_rules_test.xlsx")
+# dict = json.loads(df.to_json(orient='records', date_format="iso"))
+# print(dict)
 
-#############################################################################################################
-# basic test using testing workbook
-with ruleset("test"):
-    # antecedent
-    @when_all(m.Price > 0)
-    def say_hello(c):
-        print(f"Basic rule: ISIN is {c.m.ISIN}")
+# #############################################################################################################
+# # basic test using testing workbook
+# with ruleset("test"):
+#     # antecedent
+#     @when_all(m.Price > 0)
+#     def say_hello(c):
+#         print(f"Basic rule: ISIN is {c.m.ISIN}")
 
-for record in dict:
-    post("test", record)
+# for record in dict:
+#     post("test", record)
 
-#############################################################################################################
-# events test using example
-with ruleset('risk'):
-    @when_all(c.first << m.Price > 50,
-              c.second << m.ISIN == c.first.ISIN, 
-              c.second << m.Price > c.first.Price * 2)
-    # the event pair will only be observed once
-    def fraud(c):
-        print('Fraud detected -> {0}, {1}'.format(c.first.ID, c.second.ID))
+# #############################################################################################################
+# # events test using example
+# with ruleset('risk'):
+#     @when_all(c.first << m.Price > 50,
+#               c.second << m.ISIN == c.first.ISIN, 
+#               c.second << m.Price > c.first.Price * 2)
+#     # the event pair will only be observed once
+#     def fraud(c):
+#         print('Fraud detected -> {0}, {1}'.format(c.first.ID, c.second.ID))
 
-for record in dict:
-    post("risk", record)
+# for record in dict:
+#     post("risk", record)
 
-#############################################################################################################
-# Arrays
-# Don't know what this is doing: If change Price to 4000 it fails
+# #############################################################################################################
+# # Arrays
+# # Don't know what this is doing: If change Price to 4000 it fails
 
-with ruleset('array'):
-    # matching object array
-    @when_all(m.payments.allItems((item.Price < 250) | (item.Price >= 3000)))
-    def rule2(c):
-        print('fraud 2 detected {0}'.format(c.m.payments))
+# with ruleset('array'):
+#     # matching object array
+#     @when_all(m.payments.allItems((item.Price < 250) | (item.Price >= 3000)))
+#     def rule2(c):
+#         print('fraud 2 detected {0}'.format(c.m.payments))
      
-post('array', {'payments': dict})
+# post('array', {'payments': dict})
 
 
-#############################################################################################################
-# batching
+# #############################################################################################################
+# # batching
 
-with ruleset('expense'):
-    # this rule will trigger as soon as three events match the condition
-    @when_all(count(3), m.Price > 100)
-    def approve(c):
-        print('approved {0}'.format(c.m))
+# with ruleset('expense'):
+#     # this rule will trigger as soon as three events match the condition
+#     @when_all(count(3), m.Price > 100)
+#     def approve(c):
+#         print('approved {0}'.format(c.m))
 
-    # this rule will be triggered when 'expense' is asserted batching at most two results       
-    @when_all(cap(2),
-              c.expense << m.Price >= 100,
-              c.approval << m.review == True)
-    def reject(c):
-        print('rejected {0}'.format(c.m))
+#     # this rule will be triggered when 'expense' is asserted batching at most two results       
+#     @when_all(cap(2),
+#               c.expense << m.Price >= 100,
+#               c.approval << m.review == True)
+#     def reject(c):
+#         print('rejected {0}'.format(c.m))
 
-post_batch('expense', dict)
-assert_fact('expense', { 'review': True })
+# post_batch('expense', dict)
+# assert_fact('expense', { 'review': True })
 
 #############################################################################################################
 
